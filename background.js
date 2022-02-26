57966f0034b9e833617003bb82240d7fc38c19b2
uma <sarapan19980221@gmail.com>
2022-02-27 07:39:19 +0800

    feat: show bgColor according to star rating

--- background.js
+++ background.js
@@ -1,30 +1,64 @@
 var reviewURL = "";
+var currentURL = "";
 
+// 網頁url變化
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (
+    changeInfo.url &&
+    changeInfo.url.startsWith("https://www.google.com/maps/")
+  ) {
+    currentURL = changeInfo.url;
+    // console.log(changeInfo.url);
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { currentURL: currentURL });
+    });
+  }
+});
+
+// 監聽api
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        if(details.url.startsWith('https://www.google.com/maps/preview/review/')){
-            reviewURL = details.url;
-        }
-    }, 
-    {
-        urls: ["https://www.google.com/maps/*"]
+  function (details) {
+    if (details.url.startsWith("https://www.google.com/maps/preview/review/")) {
+      reviewURL = details.url;
     }
+  },
+  {
+    urls: ["https://www.google.com/maps/*"],
+  }
 );
 
+// 監聽錯誤
+chrome.webRequest.onErrorOccurred.addListener(
+  function (details) {
+    console.log(details.error);
+    reviewURL = "error";
+  },
+  {
+    urls: ["https://www.google.com/maps/*"],
+  }
+);
 
+// 回傳狀態
 chrome.runtime.onMessage.addListener(function (message, sender, response) {
-    switch (message.type) {
-    
-        //回傳評論的URL
-        case "getreviewURL":
-            // console.log(reviewURL);
-            response({ "reviewURL": reviewURL});
-            
-            reviewURL = "";
-            // console.log(reviewURL);
-            break;
-    
-        default:
-            console.error("Unrecognised message: ", message);
-    }
+  switch (message.type) {
+    //回傳評論的URL
+    case "getreviewURL":
+      // console.log(reviewURL);
+      response({ reviewURL: reviewURL, currentURL: currentURL });
+
+      reviewURL = ""; // Success!
+      currentURL = "";
+
+      // console.log(reviewURL);
+
+      // if(reviewURL != ""){
+      //     reviewURL = "";
+      // }
+
+      break;
+
+    default:
+      console.error("Unrecognised message: ", message);
+  }
 });
--- contentScript.js
+++ contentScript.js
@@ -1,117 +1,270 @@
 var reviewsCount = 0;
-var firstReviewURL = {
-  value: "",
-  // letMeKnow() {
-  //   console.log("The variable has changed to"+this.testVar);
-  // },
-  // get testVar() {
-  //   return this.value;
-  // },
-  set testVar(value) {
-    this.value = value;
-    // this.letMeKnow();
-    getReviews(this.value);
-    return this.value;
-  }
-}
+var firstReviewURL = "";
+var reviewsURL = "";
+var apiReturnCount = 0;
+var lastVisibleReviewIndex = 0;
 
 chrome.runtime.onMessage.addListener(function (request, sender) {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
+  // console.log(
+  //   sender.tab
+  //     ? "from a content script:" + sender.tab.url
+  //     : "from the extension"
+  // );
 
-  console.log(request.greeting);
+  // console.log(request.greeting);
 
   if (request.greeting === "hello") {
-    let button1 = document.getElementsByClassName("Yr7JMd-pane-hSRGPd");
+    var getClickBtn = setInterval(() => {
+      let reviewsBtn = document.getElementsByClassName("Yr7JMd-pane-hSRGPd");
 
-    if (button1[0] != undefined) {
-      var myStr = button1[0].getAttribute("aria-label").slice(0, -4);
-      var newStr = myStr.replace(/,/g, "");
+      if (reviewsBtn[0] != undefined) {
 
-      console.log( newStr );  // "this-is-a-test"
-      reviewsCount = Number(newStr);
-      alert("reviewsCount: " + reviewsCount);
+        clearInterval(getClickBtn);
 
-      clickReviews();
-    }
+        var myStr = reviewsBtn[0].getAttribute("aria-label").slice(0, -4);
+        var newStr = myStr.replace(/,/g, "");
+
+        console.log(newStr); // 評論數量
+        reviewsCount = Number(newStr);
+        alert("reviewsCount: " + reviewsCount);
+
+        if (reviewsCount > 3) {
+          reviewsBtn[0].click();
+          loadReviews();
+        }
+      } else {
+        console.log("等待按鈕生成");
+      }
+    }, 500);
+  } else if (request.currentURL) {
+    console.log("reviewsURL: " + reviewsURL);
+    console.log("currentURL: " + request.currentURL);
+    lastVisibleReviewIndex = 0;
+
+    // if (reviewsURL != "") {
+    //   console.log("換頁");
+    //   reviewsURL = "";
+    //   observer.disconnect();
+    // }
   }
 });
 
-function clickReviews() {
-  let reviewsBtn = document.getElementsByClassName("Yr7JMd-pane-hSRGPd");
+function loadReviews() {
+  clearInterval(getFirstReviewURL);
 
-  if (reviewsBtn[0] != undefined) {
-    clearInterval(getreviewURL);
+  var getFirstReviewURL = setInterval(() => {
+    chrome.runtime.sendMessage({ type: "getreviewURL" }, function (response) {
+      if (response.reviewURL == "error") {
+        clearInterval(getFirstReviewURL);
+        console.log("回傳錯誤");
+        firstReviewURL = "";
+        window.location.reload();
 
-    reviewsBtn[0].click();
+        loadReviews();
+      } else if (response.reviewURL != "") {
+        firstReviewURL = response.reviewURL;
+        reviewsURL = response.currentURL;
+        clearInterval(getFirstReviewURL);
+        console.log(firstReviewURL);
+        console.log(reviewsURL);
 
-    var getreviewURL = setInterval(() => {
-      chrome.runtime.sendMessage({ type: "getreviewURL" }, function (response) {
-        if (response.reviewURL != "") {
-          // firstReviewURL = response.reviewURL;
-          
-          firstReviewURL.testVar = response.reviewURL;
-          clearInterval(getreviewURL);
-          getReviews(response.reviewURL);
-        }
-      });
-    }, 500);
-  }
+        ReviewsShow();
+        getALLReviews();
+      }
+    });
+  }, 500);
 }
 
-async function getReviews(firstReviewURL){
+// 頁面顯示
+
+function ReviewsShow() {
+  clearInterval(waitReviewsShow);
+
+  var waitReviewsShow = setInterval(() => {
+    if (
+      document.getElementsByClassName("section-scrollbox")[0] &&
+      document.getElementsByClassName("section-scrollbox")[0].children.length >
+        1
+    ) {
+      console.log(
+        "目前div數量: " +
+          document.getElementsByClassName("section-scrollbox")[0].children[
+            document.getElementsByClassName("section-scrollbox")[0].children
+              .length - 2
+          ].children.length
+      );
+
+      var waitCount = 0;
+      if (reviewsCount < 10) {
+        waitCount = reviewsCount * 3 - 1;
+      } else {
+        waitCount = 29;
+      }
+      if (
+        document.getElementsByClassName("section-scrollbox")[0].children[
+          document.getElementsByClassName("section-scrollbox")[0].children
+            .length - 2
+        ].children.length >= waitCount
+      ) {
+        clearInterval(waitReviewsShow);
+        let targetDiv =
+          document.getElementsByClassName("section-scrollbox")[0].children[
+            document.getElementsByClassName("section-scrollbox")[0].children
+              .length - 2
+          ];
+        changeColor(targetDiv);
+
+        createReviewsObserver();
+      }
+    } else {
+      console.log("目前沒有div");
+    }
+  }, 500);
+}
+
+function createReviewsObserver() {
+  // try{
+  //   observer.disconnect();
+  //   console.log("have observer");
+  // }catch(e){
+  //   console.log(e);
+  //   console.log("don't have observer");
+  // }
+
+  let reviewsDiv = document.getElementsByClassName("section-scrollbox");
+
+  const targetNode = reviewsDiv[0].children[reviewsDiv[0].children.length - 2];
+
+  const config = { childList: true };
+
+  // Callback function to execute when mutations are observed
+  const callback = function (mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for (const mutation of mutationsList) {
+      if (
+        mutation.type === "childList" &&
+        mutation.target.children.length - 1 != lastVisibleReviewIndex
+      ) {
+        changeColor(mutation.target);
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+
+  // Later, you can stop observing
+  // observer.disconnect();
+}
+
+function changeColor(targetDiv) {
+  for (
+    reviewIndex = lastVisibleReviewIndex;
+    reviewIndex < targetDiv.children.length - 1;
+    reviewIndex++
+  ) {
+    let currentReview = targetDiv.children[reviewIndex];
+
+    if (currentReview.getAttribute("aria-label")) {
+      let reviewStar =
+        currentReview.children[0].children[2].children[2].children[0].children[1].getAttribute(
+          "aria-label"
+        );
+
+      // console.log(reviewStar);
+
+      let color = "#ffffff";
+
+      switch (Number(reviewStar[1])) {
+        case 1:
+          color = "#E4AEAE";
+          break;
+        case 2:
+          color = "#FAE9E9";
+          break;
+        case 4:
+          color = "#DEF0DA";
+          break;
+        case 5:
+          color = "#B3DEA9";
+          break;
+        default:
+          break;
+      }
+      currentReview.style.backgroundColor = color;
+    }
+  }
+  lastVisibleReviewIndex = targetDiv.children.length - 1;
+  console.log("目前div數量: " + lastVisibleReviewIndex);
+}
+
+// 背景取得
+
+function getALLReviews() {
   var arr = [];
   var star = [];
 
   var decade = 0;
-  if(reviewsCount % 10 > 0){
-    decade = parseInt(reviewsCount / 10)+1;
-  }else{
+  if (reviewsCount % 10 > 0) {
+    decade = parseInt(reviewsCount / 10) + 1;
+  } else {
     decade = parseInt(reviewsCount / 10);
   }
 
   for (i = 0; i < decade; i++) {
-    console.log(i);
+    let reviewURL =
+      firstReviewURL.substring(0, firstReviewURL.indexOf("!2m2!") + 7) +
+      (i * 10).toString() +
+      firstReviewURL.substring(
+        firstReviewURL.indexOf("!2m2!") + 8,
+        firstReviewURL.length
+      );
 
-    let reviewURL = firstReviewURL.substring(0,firstReviewURL.indexOf('!2m2!')+7)+(i*8).toString()+firstReviewURL.substring(firstReviewURL.indexOf('!2m2!')+8, firstReviewURL.length);
-    // console.log(firstReviewURL)
-    console.log(reviewURL)
+    // console.log(reviewURL); // 後續載入的URL
 
     fetch(reviewURL)
-    .then(function (response) {
-      return response.text();
-    })
-    .then(function (requests_result) {
-      let pretext = ")]}'";
-      let text = requests_result.replace(pretext, "");
-      let soup = JSON.parse(text);
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (requests_result) {
+        let pretext = ")]}'";
+        let text = requests_result.replace(pretext, "");
+        let soup = JSON.parse(text);
 
-      if(soup[2]){
-        // console.log(reviewURL);
-        // console.log(soup);
-        for (j = 0; j < soup[2].length; j++) {
-          arr.push(soup[2][j][3]);
-          star.push(soup[2][j][4]);
+        if (soup[2]) {
+          for (j = 0; j < soup[2].length; j++) {
+            arr.push(soup[2][j][3]);
+            star.push(soup[2][j][1]);
+          }
         }
-      }else{
-        console.log(i);
-        console.log(soup)
-      }
-    });
-    
+
+        if (apiReturnCount == decade - 1) {
+          apiReturnCount = 0;
+
+          console.log(arr);
+          console.log(star);
+
+          firstReviewURL = "";
+        } else {
+          apiReturnCount++;
+        }
+      })
+      .catch((rejected) => {
+        console.log(rejected);
+
+        var arr = [];
+        var star = [];
+        firstReviewURL = "";
+        loadReviews();
+      });
   }
-
-  console.log(arr);
-  console.log(star);
-
-  await delay(5);
 }
 
-function delay(n){
-  return new Promise(function(resolve){
-      setTimeout(resolve,n*100);
-  });
-} 
+// function delay(n){
+//   return new Promise(function(resolve){
+//       setTimeout(resolve,n*100);
+//   });
+// }
